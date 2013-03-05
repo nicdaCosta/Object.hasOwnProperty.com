1b94ee77999ccd515f58eff5ffabf612007d590e
Nic da Costa <nic@dsgnrhead.com>
2013-03-05 21:43:23 +0200

    Changed main.min.js unique  number in basket from 0.2 to 0.2.1 to cater for recent minor changes

--- js/loader.js
+++ js/loader.js
@@ -8,7 +8,7 @@ basket.require(
 
 				{ url : 'js/vendor/modernizr.min.js' , key : 'modernizr' , unique : '1.1.0' },
 				{ url : 'js/Grep.min.js' , key : 'Grep' , unique : '0.3' },
-				{ url : 'js/main.min.js' , key : 'Main' , unique : '0.2' }
+				{ url : 'js/main.min.js' , key : 'Main' , unique : '0.2.1 }
 
 			);
 
